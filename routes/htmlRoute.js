54cf4a8d8c9b6376c70592a74921d968929c738e
A Fish <ashley.paz.fish@gmail.com>
2021-09-11 17:00:42 -0500

    exercise page

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -5,3 +5,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
